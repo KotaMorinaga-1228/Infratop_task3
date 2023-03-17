07bb78f2d78010ebe654309663a51a9e6638eae2
EC2 Default User <ec2-user@ip-172-31-16-66.ap-northeast-1.compute.internal>
2023-03-17 07:38:32 +0000

    Initial commit

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,61 @@
+class BooksController < ApplicationController
+  before_action :is_matching_login_user, only: [:edit, :update, :destroy, ]
+  def create
+    @new_book = Book.new(book_params)
+    @new_book.user_id = current_user.id
+    if @new_book.save
+      flash[:notice] = "Book was successfully created."
+      redirect_to book_path(@new_book.id)
+    else
+      @books = Book.page(params[:page])
+      @user = current_user
+      render :index
+    end
+  end
+  def index
+    @new_book = Book.new
+    @books = Book.page(params[:page])
+    @user = current_user
+  end
+
+  def show
+    @book = Book.find(params[:id])
+    @new_book = Book.new
+    @user = @book.user
+  end
+  def destroy
+    @book = Book.find(params[:id])
+    @book.destroy
+    flash[:notice] = "Book was successfully destroyed."
+    redirect_to books_path
+  end
+  def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    @book = Book.find(params[:id])
+
+    if @book.update(book_params)
+      flash[:notice] = "Book was successfully updated."
+      redirect_to book_path(@book.id)
+    else
+      render :edit
+    end
+
+  end
+
+  private
+
+  def book_params
+    params.require(:book).permit(:title,:body)
+  end
+
+  def is_matching_login_user
+     book = Book.find(params[:id])
+     user =User.find(book.user_id)
+    unless user.id == current_user.id
+      redirect_to books_path
+    end
+  end
+end
